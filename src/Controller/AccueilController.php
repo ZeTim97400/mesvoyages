799ce4fb8f368906d7df396eef8e3aafc26460a2
Théotim <zetim974@gmail.com>
2024-03-12 08:54:42 +0100

    Phase 2 : premier test "Hello world !"

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/** 
+ * Description of AccueilController
+ *
+ * @author Théotim
+ */
+class AccueilController {  
+    
+    /**
+     * @Route("/", name="accueil")
+     * @return Response
+     */
+    
+    public function index(): Response{
+        return new Response('Hello world !');
+    }
+}
+
