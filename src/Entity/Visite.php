8d3d0cc7c8afc0580bca29a3de70752c6f3c0fdd
Théotim <zetim974@gmail.com>
2024-03-23 15:58:27 +0100

    Phase 15 : ORM (deuxième table 'environnement' et lien entre les deux tables, type association ManyToMany).

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\VisiteRepository;
 use DateTimeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -36,6 +38,14 @@ class Visite
 
     #[ORM\Column(nullable: true)]
     private ?int $tempmax = null;
+
+    #[ORM\ManyToMany(targetEntity: Environnement::class)]
+    private Collection $environnements;
+
+    public function __construct()
+    {
+        $this->environnements = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -131,6 +141,30 @@ class Visite
     public function setTempmax(?int $tempmax): static
     {
         $this->tempmax = $tempmax;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Environnement>
+     */
+    public function getEnvironnements(): Collection
+    {
+        return $this->environnements;
+    }
+
+    public function addEnvironnement(Environnement $environnement): static
+    {
+        if (!$this->environnements->contains($environnement)) {
+            $this->environnements->add($environnement);
+        }
+
+        return $this;
+    }
+
+    public function removeEnvironnement(Environnement $environnement): static
+    {
+        $this->environnements->removeElement($environnement);
 
         return $this;
     }
