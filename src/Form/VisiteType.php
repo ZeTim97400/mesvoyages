7693f52508724415d4cd71ae8fd6de1b40f71a29
Théotim <zetim974@gmail.com>
2024-03-23 15:27:40 +0100

    Phase 14 : ORM (ajout d'un enregistrement).

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Form;
-
 use App\Entity\Visite;
+use DateTime;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,8 +16,10 @@ class VisiteType extends AbstractType
         $builder
             ->add('ville')
             ->add('pays')
-            ->add('datecreation', null, [
+            ->add('datecreation', DateType::class, [
                 'widget' => 'single_text',
+                'data' => isset($options['date']) &&
+                    $options['data']->getDateCreation() != null ? $options['data']->getDateCreation() : new DateTime('now'),
                 'label' => 'date'
             ])
             ->add('note')
@@ -25,13 +28,11 @@ class VisiteType extends AbstractType
                 'label' =>'t° min'
             ])
             ->add('tempmax', null, [
-                'label' =>'t° min'
+                'label' =>'t° max'
             ])                      
             ->add('submit', SubmitType::class, [
                 'label' => 'Enregistrer'
-            ])
-                    
-        ;
+            ]);       
     }
 
     public function configureOptions(OptionsResolver $resolver): void
