bcb37beeb992617b99512512e8abfe32b826d326
Théotim <zetim974@gmail.com>
2024-04-01 11:33:56 +0200

    Phase 22 : codage de la page d'accueil du site.

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +11,33 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @author Théotim
  */
-class AccueilController extends AbstractController{
+class AccueilController extends AbstractController
+{
+    
+     /**
+     *
+     * @var VisiteRepository
+     */
+    private $repository;
+
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
     
     /**
      * @Route("/", name="accueil")
      * @return Response
      */
-    public function index(): Response{
-        return $this->render("pages\accueil.html.twig");
+    public function index(): Response
+    {
+        $visites = $this->repository->findAllLasted(2);
+        return $this->render("pages/accueil.html.twig", [
+            'visites' => $visites
+        ]);   
     }
-    //put your code here
 }
+
---
+++ tests/VisiteTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Description of VisiteTest
+ *
+ * @author Théotim
+ */
+class VisiteTest extends TestCase 
+{
+    
+    public function testGetDatecreationString()
+    {
+        $visite = new Visite();
+        $visite->setDatecreation(new \DateTime("2022-04-14"));
+        $this->assertEquals("14/04/2022", $visite->getDatecreationString());
+    }
+}
